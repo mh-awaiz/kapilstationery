358ed5688ee31006f536b653994692afa16fec54
Mohammed Awaiz <awaiz.web@gmail.com>
2026-02-07 13:27:11 +0530

    V19

---
+++ app/robots.js
@@ -0,0 +1,12 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/checkout", "/api"],
+      },
+    ],
+    sitemap: "https://kapilstore.in/sitemap.xml",
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = "https://kapilstore.in";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
